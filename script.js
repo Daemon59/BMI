47e798fd21e2a60b29fa9926c75987ec3132ba11
Daemon59 <124472331+Daemon59@users.noreply.github.com>
2025-06-23 18:57:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function calculateBmi() {
+
+    let weight = document.getElementById("w").value
+    let height = document.getElementById("h").value
+     //bmi formula = weight/height²
+    let bmi=  (weight) / (height/100*height/100)
+
+    document.getElementById("heading").innerHTML = "Your BMI is: "
+    document.getElementById("bmi-output").innerHTML = bmi.toFixed(2)
+
+    if (bmi <= 18.5) {
+    document.getElementById("message").innerHTML = "You are underweight"
+    }
+    else if (bmi <= 25) {
+   document.getElementById("message").innerHTML = "You are healthy"
+    }
+    else {
+    document.getElementById("message").innerHTML = "You are overweight"
+    } 
+}
+
+function reload() {
+    window.location.reload()
+}
